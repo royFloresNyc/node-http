27195fcac9d2a6237abbcc887e2728dfaf2b6c43
royFloresNyc <royfloresnyc@gmail.com>
2021-01-23 20:46:14 -0500

    Fix bug in server configuration

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 const http = require('http');
-
-const hostname = 'localhost';
-
-const port = 3000;
-
 const fs = require('fs');
 const path = require('path');
+
+const hostname = 'localhost';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
     console.log(`Request for ${req.url} by method ${req.method}`);
 
-    if (req.method == 'GET') {
+    if (req.method === 'GET') {
         var fileUrl;
         if (req.url === '/') fileUrl = '/index.html';
         else fileUrl = req.url;
 
-        var filePath = path.resolve('./publec'+fileUrl);
+        var filePath = path.resolve('./public'+fileUrl);
         const fileExt = path.extname(filePath);
         if (fileExt === '.html') {
-            fs.stat(filePath, (err, stats) => {
+            fs.access(filePath, (err) => {
                 if(err) {
                     res.statusCode = 404;
                     res.setHeader('Content-Type', 'text/html');
@@ -42,10 +40,6 @@ const server = http.createServer((req, res) => {
         res.end('<html><body><h1>Error 404: ' + req.method + 
             ' not supported</h1></body></html>');
     }
-
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>Hello, World!</h1></body></html>')
 })
 
 server.listen(port, hostname, () => {
